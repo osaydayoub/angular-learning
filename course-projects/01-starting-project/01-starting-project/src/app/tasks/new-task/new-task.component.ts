1da2b6bc136326711a0610706ab5eacfc98aa146
osaydayoub <osaydayoub@gmail.com>
2025-04-22 11:47:20 +0300

    Handle form submit

--- course-projects/01-starting-project/01-starting-project/src/app/tasks/new-task/new-task.component.ts
+++ course-projects/01-starting-project/01-starting-project/src/app/tasks/new-task/new-task.component.ts
@@ -1,15 +1,29 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   @Output() cancle = new EventEmitter<void>();
+  @Output() add = new EventEmitter<NewTaskData>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+
   onCancle() {
     this.cancle.emit();
+  }
+  onSubmit() {
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate,
+    });
   }
 }
--- course-projects/01-starting-project/01-starting-project/src/app/tasks/tasks.component.ts
+++ course-projects/01-starting-project/01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, input, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
+import { NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -48,6 +49,17 @@ export class TasksComponent {
     this.isAddingTask = true;
   }
   onCancleAddTask() {
+    this.isAddingTask = false;
+  }
+  onAddTask(taskData: NewTaskData) {
+    this.tasks.push({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date,
+    });
+
     this.isAddingTask = false;
   }
 }
