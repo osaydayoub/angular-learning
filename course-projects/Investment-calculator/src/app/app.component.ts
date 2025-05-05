21de28eaa6716beb736b481b4c7028ac6b9f48f0
osaydayoub <osaydayoub@gmail.com>
2025-05-05 20:13:40 +0300

    Add investment-results component

--- course-projects/Investment-calculator/src/app/app.component.ts
+++ course-projects/Investment-calculator/src/app/app.component.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
+import { investmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
-  onCalculateInvestmentResults(data: {
-    initialInvestment: number;
-    duration: number;
-    expectedReturn: number;
+  resultsData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
     annualInvestment: number;
-  }) {
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
+  onCalculateInvestmentResults(data: investmentInput) {
     const { initialInvestment, annualInvestment, expectedReturn, duration } =
       data;
     const annualData = [];
@@ -36,6 +41,6 @@ export class AppComponent {
       });
     }
 
-    console.log(annualData);
+    this.resultsData=annualData;
   }
 }
