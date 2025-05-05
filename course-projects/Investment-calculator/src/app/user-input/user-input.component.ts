04ff417302b9273a641dc0e789b17a998c1fe52a
osaydayoub <osaydayoub@gmail.com>
2025-05-05 22:22:23 +0300

    Using Signals & Resetting The Form After Submission

--- course-projects/Investment-calculator/src/app/user-input/user-input.component.ts
+++ course-projects/Investment-calculator/src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { investmentInput } from '../investment-input.model';
 
@@ -10,17 +10,21 @@ import { investmentInput } from '../investment-input.model';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  @Output() calculate = new EventEmitter<investmentInput>();
-  enteredInitialInvestment = '0';
-  enteredAnnualInvestment = '0';
-  enteredExpectedReturn = '5';
-  enterdDuration = '10';
+  calculate = output<investmentInput>();
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enterdDuration = signal('10');
   onSubmit() {
     this.calculate.emit({
-      initialInvestment: +this.enteredInitialInvestment,
-      duration: +this.enterdDuration,
-      expectedReturn: +this.enteredExpectedReturn,
-      annualInvestment: +this.enteredAnnualInvestment,
+      initialInvestment: +this.enteredInitialInvestment(),
+      duration: +this.enterdDuration(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      annualInvestment: +this.enteredAnnualInvestment(),
     });
+    this.enteredInitialInvestment.set('0');
+    this.enteredAnnualInvestment.set('0');
+    this.enteredExpectedReturn.set('5');
+    this.enterdDuration.set('10');
   }
 }
